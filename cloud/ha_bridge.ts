8b722d87567f34b75a629448d4daf5240703dd29
Joe Schmitt <1146681+schmittjoseph@users.noreply.github.com>
2026-07-25 07:38:51 -0700

    Fix bridge import

--- cloud/ha_bridge.ts
+++ cloud/ha_bridge.ts
@@ -17,13 +17,13 @@ import T1789EFH_F from './devices/T1789EFH_F'
 import RV13U6AM8W_D_US_WIFI from './devices/RV13U6AM8W_D_US_WIFI'
 import F3L2CYU__ from './devices/F3L2CYU__'
 import RV13B6BSD_D_US_WIFI from './devices/RV13B6BSD_D_US_WIFI'
+import WTL_FXU_BDV_NA_01 from './devices/WTL_FXU_BDV_NA_01'
 import { Device as T1Device } from './thinq1/device'
 import { Device as T2Device } from './thinq2/device'
 import { type Connection } from './homeassistant'
 import HADevice from './devices/base'
 import { type Metadata } from './thinq'
 import { AnyDevice } from './devmgr'
-import WTL_FXU_BDV_NA_01 from './devices/WTL_FXU_BDV_NA_01'
 
 type T1Factory = new (HA: Connection, thinq: T1Device, metadata: Metadata) => HADevice
 type T2Factory = new (HA: Connection, thinq: T2Device, metadata: Metadata) => HADevice
@@ -54,7 +54,7 @@ const t2deviceTypes: Record<string, T2Factory> = {
     ['RV13U6AM8W_D_US_WIFI']: RV13U6AM8W_D_US_WIFI, // LG DLE7300WE dryer
     ['F3L2CYU__']: F3L2CYU__, // LG front-load washer
     ['RV13B6BSD_D_US_WIFI']: RV13B6BSD_D_US_WIFI, // LG electric dryer
-    ['WTL_FXU_BDV_NA_01']: RV13B6BSD_D_US_WIFI, // LG WashTower
+    ['WTL_FXU_BDV_NA_01']: WTL_FXU_BDV_NA_01, // LG WashTower
 }
 
 class Bridge {
