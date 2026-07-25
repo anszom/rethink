57955109f5b5486940eb3f368aeba523bb6b6451
Joe Schmitt <1146681+schmittjoseph@users.noreply.github.com>
2026-07-25 07:18:59 -0700

    Standardize bridge

--- cloud/ha_bridge.ts
+++ cloud/ha_bridge.ts
@@ -54,7 +54,7 @@ const t2deviceTypes: Record<string, T2Factory> = {
     ['RV13U6AM8W_D_US_WIFI']: RV13U6AM8W_D_US_WIFI, // LG DLE7300WE dryer
     ['F3L2CYU__']: F3L2CYU__, // LG front-load washer
     ['RV13B6BSD_D_US_WIFI']: RV13B6BSD_D_US_WIFI, // LG electric dryer
-    WTL_FXU_BDV_NA_01, // LG WashTower
+    ['WTL_FXU_BDV_NA_01']: RV13B6BSD_D_US_WIFI, // LG WashTower
 }
 
 class Bridge {
